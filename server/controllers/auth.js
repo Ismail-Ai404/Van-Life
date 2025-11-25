a5dbf297730050834c7718e333cb0d6b6dc3c77f
Mohamad Ismail Hossain Siddiquee <73274592+Ismail-Ai404@users.noreply.github.com>
2025-11-25 17:09:46 +0600

    add auth controller logic

---
+++ server/controllers/auth.js
@@ -0,0 +1,58 @@
+/** @format */
+
+const User = require("../models/user");
+const catchAsync = require("../utils/catchAsync");
+
+// !! probably dont need this
+module.exports.renderRegisterForm = (req, res) => {
+	res.render("auth/register");
+};
+
+module.exports.registerUser = catchAsync(async (req, res, next) => {
+	const { email, username, password, confirmPassword } = req.body;
+
+	try {
+		// Create and register user
+		const user = new User({ email, username });
+		const registeredUser = await User.register(user, password);
+		req.login(registeredUser, (err) => {
+			if (err) return next(err);
+			req.flash("success", "Welcome to VanLife!");
+			res.redirect("/van");
+		});
+
+		if (!registeredUser) {
+			req.flash("error", "User could not be registered");
+			return res.redirect("/register");
+		}
+	} catch (err) {
+		req.flash("error", err.message);
+		res.redirect("/register");
+	}
+});
+// !! probably dont need this
+module.exports.renderLoginForm = (req, res) => {
+	res.render("auth/login");
+};
+
+module.exports.loginUser = (req, res) => {
+	req.flash("success", "Welcome Back!");
+	const redirectUrl = res.locals.returnTo || "/van"; // update this line to use res.locals.returnTo now
+	delete req.session.returnTo; // Clears the session value after use (prevents staleness)
+
+	// console.log(redirectUrl);
+	// console.log(res.locals.returnTo);
+	// delete req.locals.returnTo; // clean up session
+
+	res.redirect(redirectUrl);
+};
+
+module.exports.logoutUser = (req, res, next) => {
+	req.logout((err) => {
+		if (err) {
+			return next(err);
+		}
+		req.flash("success", "Goodbye!");
+		res.redirect("/campgrounds");
+	});
+};
